40041be5684c4e620112d9025482fee20f3d50d9
Aprendiz <Aprendiz>
2017-10-19 08:12:20 -0500

    conexion base datos

---
+++ src/Models/Conexion.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Models;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author Aprendiz
+ */
+public class Conexion {
+      Connection conexBD = null;
+
+    public Connection getConexBD() {
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            conexBD = DriverManager.getConnection("jdbc:mysql://localhost/CicloSanGilAPP", "root", "");
+
+        } catch (Exception e) {
+            System.out.println("Error.conexion" + e.getMessage());
+        }
+        return conexBD;
+
+    }
+}
